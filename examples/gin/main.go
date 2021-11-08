b6d9c0755b768bf76d21f5fa407e5818e25f6661
zhuxiongwei <zhuxiongwei@bytedance.com>
2021-11-08 21:21:14 +0800

    add Gin Example

---
+++ examples/gin/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+	})
+
+	baseGroup := r.Group("/base")
+	BaseRouteGroup(baseGroup)
+
+	r.Run(":8888")
+}
